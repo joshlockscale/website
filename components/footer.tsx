8216a94135da7204bbcb10c1a22f672130cbdc00
joshlockscale <your.email@example.com>
2025-04-09 21:48:17 +0200

    Adjust logo size and spacing for better alignment

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,7 +8,7 @@ export function Footer() {
       <div className="container flex items-center justify-between gap-x-10 gap-y-5 flex-wrap">
         <div className="flex-1">
           <div className="flex items-center gap-2">
-            <img src="/images/logo.png" alt="Lockscale Logo" className="h-12 w-auto mr-4" />
+            <img src="/images/logo.png" alt="Lockscale Logo" className="h-10 w-auto mr-2" />
             <span className="font-heading text-xl font-extrabold">Lockscale</span>
           </div>
         </div>
